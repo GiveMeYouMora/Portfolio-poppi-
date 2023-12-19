e448972e89120fe480d2b2b2996dd91b26e70feb
GiveMeYourMora <125956689+GiveMeYouMora@users.noreply.github.com>
2023-12-19 20:00:33 +1000

    Add files via upload

---
+++ portfolio/assets/js/index.js
@@ -0,0 +1,18 @@
+console.log("One peace")
+var name="73"
+let name_2= "5 6"
+const name_const = "7 8"
+console.log(name, name_2, name_const); 
+let age=prompt("Введите возраст")
+if (age>=18) {
+    "Можно"
+} else {
+    "Нельзя"
+}
+for (let i = 0; i < 10; i++) {
+    console.log(i);    
+}
+let a = 0 
+while (a<10) {
+    console.log(a++)
+}
