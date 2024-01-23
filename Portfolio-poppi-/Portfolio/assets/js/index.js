3c83b143777ee5bd52397ac5bdcfbabb9929d5d6
GiveMeYourMora <125956689+GiveMeYouMora@users.noreply.github.com>
2024-01-23 19:56:13 +1000

    Add files via upload

--- Portfolio-poppi-/Portfolio/assets/js/index.js
+++ Portfolio-poppi-/Portfolio/assets/js/index.js
@@ -16,3 +16,24 @@
 // while (a<10) {
 //     console.log(a++)
 // }
+let string = `строка`
+let number =28
+let bull= false
+let nothink = null
+let underfind 
+let name 
+let nun
+let persone 
+let sim = 'l'
+let h1 = document.getElementsByTagName("h1")
+console.log("h1")
+
+
+let baground =document.querySelector("body")
+
+console.log(baground.style.backgroundImage);
+let check = document.getElementById("lightmode")
+check.onclick = function(){
+    baground.style.backgroundImage = 'url("./assets/images/wave.png")';
+    console.log("dsfsfsfs")
+};
