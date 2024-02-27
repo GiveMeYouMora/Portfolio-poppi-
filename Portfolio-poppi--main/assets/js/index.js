3eea4100736ebb48e8e6e0eba2b6fe2455d1986d
GiveMeYourMora <125956689+GiveMeYouMora@users.noreply.github.com>
2024-02-27 19:58:18 +1000

    Add files via upload

---
+++ Portfolio-poppi--main/assets/js/index.js
@@ -0,0 +1,105 @@
+// let lived = document.getElementsByTagName('div')[0]
+// console.log(lived);
+// let loved = document.querySelector('.block')
+// console.log(loved);
+// let luved = document.getElementById('blockid')
+// console.log(luved);
+// lived.style.height = '200px'
+// lived.style.width = '50px'
+// lived.style.borderRadius = '10px'
+// lived.style.backgroundColor = '#5671a6'
+// loved.style.height = '200px'
+// loved.style.width = '50px'
+// loved.style.borderRadius = '10px'
+// loved.style.backgroundColor = '#383838'
+// luved.style.height = '200px'
+// luved.style.width = '50px'
+// luved.style.borderRadius = '10px'
+// luved.style.backgroundColor = '#6d8093'
+
+// console.log(lived.classList);
+// lived.classList.add('--active')
+// loved.classList.remove('fgfdg')
+
+// console.log("One peace")
+// var name="73"
+// let name_2= "5 6"
+// const name_const = "7 8"
+// console.log(name, name_2, name_const); 
+// let age=prompt("Введите возраст")
+// if (age>=18) {
+//     "Можно"
+// } else {
+//     "Нельзя"
+// }
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);    
+// }
+// let a = 0 
+// while (a<10) {
+//     console.log(a++)
+// }
+let string = `строка`
+let number =28
+let bull= false
+let nothink = null
+let underfind 
+let name 
+let nun
+let persone 
+let sim = 'l'
+let h1 = document.getElementsByTagName("h1")
+console.log("h1")
+
+
+let baground =document.querySelector("body")
+
+console.log(baground.style.backgroundImage);
+let check = document.getElementById("lightmode")
+let block = document.querySelector('.block');
+
+let laved = false;
+
+let lightmode = true
+
+check.onclick = function() {
+
+    console.log(check.checked);
+   if (check.checked == false) (
+        baground.style.backgroundImage = 'url(./assets/images/background-light.png)' 
+   )
+   else (
+        baground.style.backgroundImage = 'url(./assets/images/wave.png)'
+   )
+   }
+let massCards = [
+{
+    title:'Заголовок',
+    text:'text',
+    data:'30.01.2024',
+    isPublick:false,
+},
+];
+let cardlist = document.querySelector(".cardlist");
+cardlist.insertAdjacentHTML("beforeend",`<h1>Карточка<h1>`)
+for (let i = 0; i<massCards.length; i++) {
+     cardlist.insertAdjacentHTML(
+          "afterbegin",`      
+</div>
+<h2>Феличита</h2>
+<p>Выйду на улицу</p>
+<p>30.01.2024</p>
+ </div>`)
+}
+const Timeout = setTimeout(function()(
+  console.log("Сработала задержка")
+), 5000);
+const TimeInterval = setInterval(() => {
+     console.log("Сработал интервал")
+}, 1000);
+//clearInterval(TimeInterval)
+anin.onclick = function ()(
+             setInterval(() => {
+              anin.style.left = position + "px";
+              position =+ 1
+            }, 20));
